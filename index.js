64b65d2f7e165257d7f8c0fda25b6f1c79df5906
jonathans199 <jso1919@gmail.com>
2022-07-06 20:49:26 -0400

    updated home

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/home/home.html'))
 })
+
+// team 1 - /about route and serve /about/about.html
+// team 2 - /contact route and serve /contact/contact.html
+// team 3 - /services route and serve /services/services.html
+// team 4 - /products route and serve /products/products.html
+// team 5 - /login route and serve /login/login.html
